5b54e816800b099478e3caa6d249662e12d5e41a
gin <73906888+gingkapls@users.noreply.github.com>
2024-05-17 03:27:09 +0530

    npm: init, add eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  { ignores: ["*spec.js"] },
   pluginJs.configs.recommended,
 ];
